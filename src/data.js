faa0bb8b5c224583d61a6993347b4a6381cd5109
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2019-03-26 09:12:03 -0500

    subiendo la página hasta ahora, falta estadistica y responsive y contenido de contact

---
+++ src/data.js
@@ -0,0 +1,118 @@
+//
+
+let showBoats = INJURIES.map(function(i) {
+  return i.Year + " " + i.Total_Injured_Persons_Recreational_Boating;
+});
+window.showBoats=showBoats;
+
+let newarrayTrains=[];
+const showTrains=(INJURIES)=>{
+  for(let i=0;i<INJURIES.length;i++){
+    newarrayTrains += [INJURIES[i].Year+" " + INJURIES[i].Total_Injured_Persons_Train_Accidents_Rail_Roads + " "];
+  }
+  return newarrayTrains;
+};
+window.trains=showTrains;
+
+
+let newarrayYear=[];
+const showAccidentsByYear=(array,option)=>{
+  for(let i=0;i<array.length;i++){
+  if(array[i].Year===option){
+    newarrayYear=[array[i].Year+" "+array[i].Total_Injured_Persons_Recreational_Boating+" " +array[i].Total_Injured_Persons_Train_Accidents_Rail_Roads];
+  }
+  }
+  return newarrayYear;
+};
+window.showAccidentsByYear=showAccidentsByYear;
+
+function sortArrs(a, b) {
+  if (a > b) {
+    return 1;
+  } else if (a < b) {
+    return -1;
+  } else if (a === b) {
+    return 0;
+  }
+}
+window.sortArrs=sortArrs;
+// //CÁLCULOS
+// //aproximación en % de acuerdo al total de los números de accidentados por Trains en años(EEUU)
+//  let newarrayTrain= [{quantity :0},{quantity :0},{quantity :0},{quantity :0},{quantity :665},{quantity :476}
+//   ,{quantity :451},{quantity :326},{quantity :171},{quantity :308},{quantity :262},{quantity :294},{quantity :281}
+//   ,{quantity :183},{quantity :129},{quantity :130},{quantity :275},{quantity :310},{quantity :1884},{quantity :232}
+//   ,{quantity :346},{quantity :787},{quantity :220},{quantity :309},{quantity :321},{quantity :127},{quantity :110}
+//   ,{quantity :217},{quantity :462},{quantity :328},{quantity :139},{quantity :558},{quantity :284}];
+// let totalLocation = 0;
+// newarrayTrain.forEach(element => {
+//   totalLocation += element.quantity;
+// })
+// newarrayTrain.forEach(element => {
+//  console.log(element.quantity * 100 / totalLocation)
+// })
+
+// //aproximación en % de acuerdo al total de los números de accidentados por Boating en años(EEUU)
+// let newarrayBoat= [{quantity :929},{quantity :927},{quantity :780},{quantity :2136},{quantity :2650},{quantity :2757}
+//   ,{quantity :3822},{quantity :3967},{quantity :3683},{quantity :3559},{quantity :4084},{quantity :4141},{quantity :4442}
+//   ,{quantity :4555},{quantity :4612},{quantity :4315},{quantity :4355},{quantity :4274},{quantity :4062},{quantity :3888}
+//   ,{quantity :3363},{quantity :3451},{quantity :3474},{quantity :3673},{quantity :3331},{quantity :3358},{quantity : 3153}
+//   ,{quantity :3081},{quantity :3000},{quantity :2620},{quantity :2678},{quantity :2679},{quantity :0}];
+// let totalBoa = 0;
+// newarrayBoat.forEach(element => {
+//   totalBoa += element.quantity;
+// })
+// newarrayBoat.forEach(element => {
+//  console.log(element.quantity * 100 / totalLocation)
+// })
+
+
+
+// //Suma de personas accidentadas por Trains en EEUU
+//   let sumaTrains=0;
+// for(let i=0; i < INJURIES.length; i++){
+  
+//   sumaTrains += INJURIES[i].Total_Injured_Persons_Train_Accidents_Rail_Roads;
+// }
+
+// console.log('Suma Total_Injured_Persons_Train_Accidents_Rail_Roads');
+// console.log(sumaTrains);
+
+// //Suma de personas accidentadas por Boating en EEUU
+
+
+// let sumaBoats=0;
+// for(let i=0; i < INJURIES.length; i++){
+  
+//   sumaBoats += INJURIES[i].Total_Injured_Persons_Recreational_Boating;
+// }
+
+// console.log('Suma Total_Injured_Persons_Recreational_Boating');
+// console.log(sumaBoats);
+
+
+
+// //HALLANDO EL MÁXIMO Y/O MÍNIMO DEL NÚMERO DE ACCIDENTES EN BOATING
+// //El máximo
+
+//     maxBoats = 0;
+
+// for(let i=0;i<INJURIES.length;i++){
+//     if(maxBoats < INJURIES[i].Total_Injured_Persons_Recreational_Boating){
+//         maxBoats = INJURIES[i].Total_Injured_Persons_Recreational_Boating;
+//     }
+// }
+// console.log('Máximo Total_Injured_Persons_Recreational_Boating');
+// console.log(maxBoats); //4612
+
+
+// //HALLANDO EL MÁXIMO Y/O MÍNIMO DEL NÚMERO DE ACCIDENTES EN TRAINS
+// //El máximo
+// maxTrains = 0;
+
+// for(let i=0;i<INJURIES.length;i++){
+//     if(maxTrains < INJURIES[i].Total_Injured_Persons_Train_Accidents_Rail_Roads){
+//         maxTrains = INJURIES[i].Total_Injured_Persons_Train_Accidents_Rail_Roads;
+//     }
+// }
+// console.log('Máximo Total_Injured_Persons_Train_Accidents_Rail_Roads');
+// console.log(maxTrains); //1884
---
+++ src/main.js
@@ -0,0 +1,185 @@
+
+const homeButton = document.getElementById("home-menu-btn");
+const aboutButton = document.getElementById("about-menu-btn");
+const contactButton = document.getElementById("contact-menu-btn");
+const typeButton = document.getElementById("tipo-menu-btn");
+const yearButton = document.getElementById("año-menu-btn");
+const homeScreen = document.getElementById("home-screen");
+const statisticScreen = document.getElementById("statistic-screen");
+const contactScreen = document.getElementById("contact-us-screen");
+const typeScreen = document.getElementById("type-accident-screen");
+const yearScreen = document.getElementById("year-accident-screen");
+const trainScreen = document.getElementById("train-screen");
+const trainBtn = document.getElementById("train");
+const boatScreen = document.getElementById("boat-screen");
+const boatBtn = document.getElementById("boat");
+const seeDataBtn = document.getElementById("see-data");
+const sortBtnTrain = document.getElementById("sort-por-año");
+const sortBtnBoat = document.getElementById("sort-por-año-boat");
+
+statisticScreen.style.display = "none";
+contactScreen.style.display = "none";
+typeScreen.style.display = "none";
+yearScreen.style.display = "none";
+boatScreen.style.display = "none";
+trainScreen.style.display = "none";
+
+
+
+homeButton.addEventListener("click", () => {
+  homeScreen.style.display = "flex";
+  statisticScreen.style.display = "none";
+  contactScreen.style.display = "none";
+  typeScreen.style.display = "none";
+  boatScreen.style.display = "none";
+  yearScreen.style.display = "none";
+  trainScreen.style.display = "none";
+
+});
+
+aboutButton.addEventListener("click", () => {
+  homeScreen.style.display = "none";
+  contactScreen.style.display = "none";
+  typeScreen.style.display = "none";
+  yearScreen.style.display = "none";
+  boatScreen.style.display = "none";
+  trainScreen.style.display = "none";
+  statisticScreen.style.display = "block";
+});
+
+contactButton.addEventListener("click", () => {
+  homeScreen.style.display = "none";
+  typeScreen.style.display = "none";
+  yearScreen.style.display = "none";
+  statisticScreen.style.display = "none";
+  boatScreen.style.display = "none";
+  trainScreen.style.display = "none";
+  contactScreen.style.display = "block";
+});
+
+typeButton.addEventListener("click", () => {
+  homeScreen.style.display = "none";
+  contactScreen.style.display = "none";
+  yearScreen.style.display = "none";
+  statisticScreen.style.display = "none";
+  boatScreen.style.display = "none";
+  trainScreen.style.display = "none";
+  typeScreen.style.display = "block";
+});
+
+yearButton.addEventListener("click", () => {
+  homeScreen.style.display = "none";
+  contactScreen.style.display = "none";
+  typeScreen.style.display = "none";
+  statisticScreen.style.display = "none";
+  boatScreen.style.display = "none";
+  trainScreen.style.display = "none";
+  yearScreen.style.display = "block";
+});
+
+trainBtn.addEventListener("click", () => {
+  homeScreen.style.display = "none";
+  contactScreen.style.display = "none";
+  typeScreen.style.display = "none";
+  statisticScreen.style.display = "none";
+  yearScreen.style.display = "none";
+  boatScreen.style.display = "none";
+  trainScreen.style.display = "block";
+  pintarTrain(arrInjuredPeople);
+});
+//pintando data
+const containerTrain = document.getElementById("data-train");
+const arrInjuredPeople = INJURIES;
+const pintarTrain = (arrInjuredPeople) => {
+  let string = "";
+  for (let i = 0; i < arrInjuredPeople.length; i++) {
+    string += ` 
+        <div class="row display-inline-block">
+          <div class="column">
+            <div class="boxes-design-type display-inline-block">
+            <p>Year: ${arrInjuredPeople[i].Year}</p>
+            <p>#: ${arrInjuredPeople[i].Total_Injured_Persons_Train_Accidents_Rail_Roads}</p>
+            </div>
+          </div>
+        </div>`
+  }
+  containerTrain.innerHTML = string;
+};
+
+sortBtnTrain.addEventListener("click", () => {
+  arrInjuredPeople.sort(function (a, b) {
+    let z = a.Year;
+    let f = b.Year;
+    return z > f ? -1 : z < f ? 1 : 0;
+  });
+  pintarTrain(arrInjuredPeople)
+})
+
+boatBtn.addEventListener("click", () => {
+  homeScreen.style.display = "none";
+  contactScreen.style.display = "none";
+  typeScreen.style.display = "none";
+  statisticScreen.style.display = "none";
+  yearScreen.style.display = "none";
+  trainScreen.style.display = "none";
+  boatScreen.style.display = "block";
+  pintarBoat(arrInjuredPeopleBoat);
+});
+
+const containerBoat = document.getElementById("data-boats");
+const arrInjuredPeopleBoat = INJURIES;
+const pintarBoat = (arrInjuredPeopleBoat) => {
+  let string = "";
+  for (let i = 0; i < arrInjuredPeopleBoat.length; i++) {
+    string += `
+        <div class="row display-inline-block">
+          <div class="column">
+            <div class="boxes-design-type display-inline-block">
+            <p>Year: ${arrInjuredPeopleBoat[i].Year}</p>
+            <p>#: ${arrInjuredPeopleBoat[i].Total_Injured_Persons_Recreational_Boating}</p>
+            </div>
+          </div>
+        </div>`
+  }
+  containerBoat.innerHTML = string;
+};
+
+sortBtnBoat.addEventListener("click", () => {
+  arrInjuredPeopleBoat.sort(function (a, b) {
+    let x = a.Total_Injured_Persons_Recreational_Boating;
+    let y = b.Total_Injured_Persons_Recreational_Boating;
+    return x > y ? -1 : x < y ? 1 : 0;
+  });
+  pintarBoat(arrInjuredPeopleBoat);
+})
+
+seeDataBtn.addEventListener("click", () => {
+  const option = document.getElementById("Years").value;
+  pintarYear(arrYearInjuredpeople, option);
+});
+
+const yearData = document.getElementById("year-data");
+const arrYearInjuredpeople = INJURIES;
+const pintarYear = (INJURIES, option) => {
+  let string = "";
+  for (let i = 0; i < INJURIES.length; i++) {
+    if (INJURIES[i].Year === option) {
+      string += `
+          <div class="center-items">
+            <div class="box-type-year display-inline space text-align-center ">
+              <p>Trains</p>
+              </br>
+              <p>Year: ${INJURIES[i].Year}</p>
+              <p>#: ${INJURIES[i].Total_Injured_Persons_Recreational_Boating}</p>
+            </div>
+            <div class="box-type-year display-inline text-align-center ">
+              <p>Boats</p>
+              </br>
+              <p>Year: ${INJURIES[i].Year}</p>
+              <p>#: ${INJURIES[i].Total_Injured_Persons_Train_Accidents_Rail_Roads}</p>
+            </div>
+          </div>`
+    }
+  }
+  yearData.innerHTML = string;
+}
