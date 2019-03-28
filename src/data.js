87140c7703678e0fe12b97323d29f4877a19ab0d
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2019-03-28 09:49:33 -0500

    agregando los esilos para responsive

--- src/data.js
+++ src/data.js
@@ -1,51 +1,69 @@
 //showArrTrain y Boat son para crear un nuevo arrays solo con los elementos de tren y bote
-const showArrTrain = (data)=>{ 
-  let newdatat=[];
-  for(let i=0;i<data.length;i++){
+const showArrTrain = (data) => {
+  let newdatat = [];
+  for (let i = 0; i < data.length; i++) {
     newdatat.push({
-      'Year':data[i].Year,
+      'Year': data[i].Year,
       'accidentstrain': data[i]['Total_Injured_Persons_Train_Accidents_Rail_Roads']
     })
   }
   return newdatat;
 }
-window.showArrTrain=showArrTrain;
+window.showArrTrain = showArrTrain;
 
-const showArrBoat = (data)=>{ 
-  let newdatab=[];
-  for(let i=0;i<data.length;i++){
+const showArrBoat = (data) => {
+  let newdatab = [];
+  for (let i = 0; i < data.length; i++) {
     newdatab.push({
-      'Year':data[i].Year,
+      'Year': data[i].Year,
       'accidentsboat': data[i]['Total_Injured_Persons_Recreational_Boating']
     })
   }
   return newdatab;
 }
-window.showArrBoat=showArrBoat;
+window.showArrBoat = showArrBoat;
 
-const filterData=(data,condition)=>{
-  let filteredArr=[];
-  for(let i=0;i<data.length;i++){
-    if(data[i].Year===condition){
+const filterData = (data, condition) => {
+  let filteredArr = [];
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].Year === condition) {
       filteredArr.push({
-        'Year':data[i].Year,
-        'Trains':data[i].Total_Injured_Persons_Train_Accidents_Rail_Roads,
-        'Boats':data[i].Total_Injured_Persons_Recreational_Boating
+        'Year': data[i].Year,
+        'Trains': data[i].Total_Injured_Persons_Train_Accidents_Rail_Roads,
+        'Boats': data[i].Total_Injured_Persons_Recreational_Boating
       })
     }
   }
   return filteredArr;
 }
-window.filterData=filterData;
+window.filterData = filterData;
 
+const sortData = (data, sortBy, sortOrder) => {
+  const orderedArr = data.sort=(a,b)=>{
+    if(a[sortBy]> b[sortBy]){
+      return 1;
+    }else if (a[sortBy] <b [sortBy]) {
+      return -1;
+    } else if (a[sortBy] === b[sortBy]) {
+      return 0;
+    }
+  };
+  if(sortOrder == "A"){
+    return orderedArr;
+  } else if(sortOrder=="D"){
+    return orderedArr.reverse();
+  }
+}
+window.sortData=sortData;
 
-const computeStats=(data)=>{
-  let maximo=0;
-  for(let i=0;i<data.length;i++){
-    if(maximo<data[i].Total_Injured_Persons_Recreational_Boating){
-     maximo=data[i].Total_Injured_Persons_Recreational_Boating;
+const computeStats = (data) => {
+  let maximoBoats = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (maximoBoats < data[i].Total_Injured_Persons_Recreational_Boating) {
+      maximoBoats = data[i].Total_Injured_Persons_Recreational_Boating;
     }
   }
-  return maximo;
+  return [maximoBoats];
 }
-window.computeStats=computeStats;
+window.computeStats = computeStats;
+
--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 
 const homeButton = document.getElementById("home-menu-btn");
 const aboutButton = document.getElementById("about-menu-btn");
-const contactButton = document.getElementById("contact-menu-btn");
 const typeButton = document.getElementById("tipo-menu-btn");
 const yearButton = document.getElementById("año-menu-btn");
 const homeScreen = document.getElementById("home-screen");
 const statisticScreen = document.getElementById("statistic-screen");
-const contactScreen = document.getElementById("contact-us-screen");
 const typeScreen = document.getElementById("type-accident-screen");
 const yearScreen = document.getElementById("year-accident-screen");
 const trainScreen = document.getElementById("train-screen");
@@ -14,11 +12,10 @@ const trainBtn = document.getElementById("train");
 const boatScreen = document.getElementById("boat-screen");
 const boatBtn = document.getElementById("boat");
 const seeDataBtn = document.getElementById("see-data");
-const sortBtnTrain=document.getElementById("sort-por-año");
-const sortBtnBoat=document.getElementById("sort-por-año-boat");
+const sortBtnTrain = document.getElementById("sort-por-año");
+const sortBtnBoat = document.getElementById("sort-por-año-boat");
 
 statisticScreen.style.display = "none";
-contactScreen.style.display = "none";
 typeScreen.style.display = "none";
 yearScreen.style.display = "none";
 boatScreen.style.display = "none";
@@ -29,17 +26,15 @@ trainScreen.style.display = "none";
 homeButton.addEventListener("click", () => {
   homeScreen.style.display = "block";
   statisticScreen.style.display = "none";
-  contactScreen.style.display = "none";
   typeScreen.style.display = "none";
   boatScreen.style.display = "none";
-  yearScreen.style.display="none";
+  yearScreen.style.display = "none";
   trainScreen.style.display = "none";
 
 });
 
 aboutButton.addEventListener("click", () => {
   homeScreen.style.display = "none";
-  contactScreen.style.display = "none";
   typeScreen.style.display = "none";
   yearScreen.style.display = "none";
   boatScreen.style.display = "none";
@@ -47,19 +42,9 @@ aboutButton.addEventListener("click", () => {
   statisticScreen.style.display = "block";
 });
 
-contactButton.addEventListener("click", () => {
-  homeScreen.style.display = "none";
-  typeScreen.style.display = "none";
-  yearScreen.style.display = "none";
-  statisticScreen.style.display = "none";
-  boatScreen.style.display = "none";
-  trainScreen.style.display = "none";
-  contactScreen.style.display = "block";
-});
 
 typeButton.addEventListener("click", () => {
   homeScreen.style.display = "none";
-  contactScreen.style.display = "none";
   yearScreen.style.display = "none";
   statisticScreen.style.display = "none";
   boatScreen.style.display = "none";
@@ -69,7 +54,6 @@ typeButton.addEventListener("click", () => {
 
 yearButton.addEventListener("click", () => {
   homeScreen.style.display = "none";
-  contactScreen.style.display = "none";
   typeScreen.style.display = "none";
   statisticScreen.style.display = "none";
   boatScreen.style.display = "none";
@@ -79,7 +63,6 @@ yearButton.addEventListener("click", () => {
 
 trainBtn.addEventListener("click", () => {
   homeScreen.style.display = "none";
-  contactScreen.style.display = "none";
   typeScreen.style.display = "none";
   statisticScreen.style.display = "none";
   yearScreen.style.display = "none";
@@ -87,6 +70,7 @@ trainBtn.addEventListener("click", () => {
   trainScreen.style.display = "block";
   pintarTrain(showArrTrain(INJURIES));
 });
+
 //pintando data
 const containerTrain = document.getElementById("data-train");
 
@@ -106,18 +90,13 @@ const pintarTrain = (data) => {
   containerTrain.innerHTML = string;
 };
 
-sortBtnTrain.addEventListener("click", ()=>{
-  arrInjuredPeople.sort(function(a,b){
-    let z=a.Year;
-    let f=b.Year;
-    return z>f?-1:z<f?1:0;
-  });
-  pintarTrain(arrInjuredPeople)
+sortBtnTrain.addEventListener("click", () => {
+  pintarTrain(sortData(INJURIES,"Year","D"));
 })
 
 boatBtn.addEventListener("click", () => {
   homeScreen.style.display = "none";
-  contactScreen.style.display = "none";
+
   typeScreen.style.display = "none";
   statisticScreen.style.display = "none";
   yearScreen.style.display = "none";
@@ -143,19 +122,14 @@ const pintarBoat = (data) => {
   containerBoat.innerHTML = string;
 };
 
-sortBtnBoat.addEventListener("click", ()=>{
-  arrInjuredPeopleBoat.sort(function(a,b){
-    let x=a.Total_Injured_Persons_Recreational_Boating;
-    let y=b.Total_Injured_Persons_Recreational_Boating;
-    return x>y?-1:x<y?1:0;
-  });
-  pintarBoat(arrInjuredPeopleBoat);
-})
-
-seeDataBtn.addEventListener("click", () => {
-  const option = document.getElementById("Years").value;
-  pintarYear(filterData(INJURIES,option));
-});
+// sortBtnBoat.addEventListener("click", () => {
+//   arrInjuredPeopleBoat.sort(function (a, b) {
+//     let x = a.Total_Injured_Persons_Recreational_Boating;
+//     let y = b.Total_Injured_Persons_Recreational_Boating;
+//     return x > y ? -1 : x < y ? 1 : 0;
+//   });
+//   pintarBoat(arrInjuredPeopleBoat);
+// })
 
 const yearData = document.getElementById("year-data");
 const pintarYear = (data) => {
@@ -176,17 +150,22 @@ const pintarYear = (data) => {
               <p>#: ${data[i].Trains}</p>
             </div>
           </div>`
-    }
-    yearData.innerHTML = string;
   }
-  
-const maxbtn=document.getElementById("max");
-maxbtn.addEventListener("click",()=>{
+  yearData.innerHTML = string;
+}
+
+seeDataBtn.addEventListener("click", () => {
+  const option = document.getElementById("Years").value;
+  pintarYear(filterData(INJURIES, option));
+});
+
+const maxbtn = document.getElementById("max");
+maxbtn.addEventListener("click", () => {
   pintarMax(computeStats(INJURIES));
 })
-const stats=document.getElementById("stats");
-const pintarMax=(maximo)=>{
-  let string="";
-    string += ` ${maximo}`
-stats.innerHTML=string;
+const stats = document.getElementById("stats");
+const pintarMax = (maximoBoats) => {
+  let string = "";
+  string += ` ${maximoBoats}`
+  stats.innerHTML = string;
 }
